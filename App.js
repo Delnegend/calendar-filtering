20724b387df1d59909324db3c0e16279fba9d35d
Delnegend <kiennguyen19323@gmail.com>
2021-11-19 07:36:00 +0700

    R1.0

--- App.js
+++ App.js
@@ -2,12 +2,12 @@
 const axios = require('axios');
 const express = require('express');
 const clone = require('just-clone');
-
 const ical = require('ical-generator');
-const { icalParser } = require('./utils/icalParser');
-const { RRule } = require('rrule');
 
-// Express server
+const { convert } = require('./utils/icalToFromJson');
+const parseUserConfig = require('./utils/parseUserConfig');
+const createEventConfig = require('./utils/createEventConfig');
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
@@ -15,91 +15,54 @@ app.listen(PORT, () => {
 });
 
 app.get('/', async (req, res) => {
-  // #region
+  let userConfig = parseUserConfig(req.query.userConfig);
 
-  // let { config } = req.query;
-  // if (!config) {
-  /* eslint-disable quotes, comma-dangle, semi */
-  let config = {
-    "userCal": false,
-    "selectCal": "B1_L1",
-    "type": "blist",
-    "matchPart": [
-      "[B1.09]",
-      "[B1.10]",
-      // "[B1.11]",
-      "[B1.12]",
-      "[B1.13]",
-      "[B1.14]",
-      "[B1.15]",
-      "[B1.16]",
-      "L1.1",
-      "L1.2",
-      "L1.3",
-      "L1.4",
-      // "L1.5",
-      "L1.6",
-      "L1.7",
-    ],
-    "matchString": [
-      "English B1",
-      "Semester I",
-      "English"
-    ]
-  }
-  /* eslint-enable quotes, comma-dangle, semi */
-  // }
-  const calURL = {
-    'B1_L1': 'https://calendar.google.com/calendar/ical/v0qbbfube10coopf5vfiovf5qo%40group.calendar.google.com/public/basic.ics',
-    'B1_L2': 'https://calendar.google.com/calendar/ical/c_gj0263fted8l7qfm15shi2069c%40group.calendar.google.com/public/basic.ics',
-    'B1_L3': 'https://calendar.google.com/calendar/ical/c_fsjo0lba0dfq79odbo46jer5fg%40group.calendar.google.com/public/basic.ics',
-    'dev': 'http://localhost:5500/basic.ics',
-  };
-
-  let calendarURL;
-  if (config.userCal) calendarURL = config.userCal;
-  else calendarURL = calURL[config.selectCal];
-
-  // FOR DEVELOPMENT PURPOSES ONLY
-  calendarURL = calURL.dev;
-  // #endregion
-
-  let rawCal = await axios.get(calendarURL);
-  let jsonCal = icalParser(rawCal.data);
-  let events = clone(jsonCal.events);
-
-  if (config.type == 'blist') {
-    for (const item of config.matchPart) events = events.filter((event) => !event.summary?.value.includes(item));
-    for (const item of config.matchString) events = events.filter((event) => !(event.summary?.value == item));
+  let rawCal = await axios.get(userConfig.url);
+  let jsonCal = convert(rawCal.data);
+  let events = clone(jsonCal.VCALENDAR[0].VEVENT);
+  if (userConfig.type == 'blist') {
+    if (userConfig.matchPart) {
+      for (const item of userConfig.matchPart) events = events.filter((event) => !event.SUMMARY.includes(item));
+    }
+    if (userConfig.matchString) {
+      for (const item of userConfig.matchString) events = events.filter((event) => !(event.SUMMARY == item));
+    }
   }
 
-  // #region
   const newCalendar = ical({
     'prodId': '//Google Inc//Google Calendar//EN',
-    'method': jsonCal.calendarData?.method,
-    'name': 'Kien Calendar',
-    // 'timezone': jsonCal.calendarData['x-wr-timezone'],
+    'name': userConfig.name,
+    'description': jsonCal.VCALENDAR[0]['X-WR-CALDESC'],
+    'scale': jsonCal.VCALENDAR[0]?.CALSCALE,
+    'method': jsonCal.VCALENDAR[0]?.METHOD,
   });
-
   for (const event of events) {
-    newCalendar.createEvent({
-      'start': new Date(event.dtstart?.value),
-      'end': new Date(event.dtend?.value),
-      'id': event.uid?.value,
-      'description': event.description?.value,
-      'location': event.location?.value,
-      'sequence': event.sequence?.value,
-      'status': event.status?.value,
-      'summary': event.summary?.value,
-      'transparency': event.transp?.value,
-      // 'timezone': jsonCal.calendarData['x-wr-timezone'],
-      'repeating': new RRule(event.recurrenceRule?.options).toString(),
-    });
+    const eventConfig = createEventConfig(event);
+    if (userConfig.limit?.start) {
+      const condition = [
+        parseInt(eventConfig.date.start.year) >= userConfig.limit.start.year,
+        parseInt(eventConfig.date.start.month) >= userConfig.limit.start.month,
+        parseInt(eventConfig.date.start.day) >= userConfig.limit.start.day,
+      ];
+      if (!condition.includes(false)) newCalendar.createEvent(eventConfig.data);
+    } else if (userConfig.limit?.start && userConfig.limit?.end) {
+      const condition = [
+        parseInt(eventConfig.date.start.year) >= userConfig.limit.start.year,
+        parseInt(eventConfig.date.start.month) >= userConfig.limit.start.month,
+        parseInt(eventConfig.date.start.day) >= userConfig.limit.start.day,
+        parseInt(eventConfig.date.end.year) <= userConfig.limit.end.year,
+        parseInt(eventConfig.date.end.month) <= userConfig.limit.end.month,
+        parseInt(eventConfig.date.end.day) <= userConfig.limit.end.day,
+      ];
+      if (!condition.includes(false)) newCalendar.createEvent(eventConfig.data);
+    } else {
+      newCalendar.createEvent(eventConfig.data);
+    }
   }
 
   // FOR DEVELOPMENT PURPOSES ONLY
   // res.setHeader('Content-type', 'application/json');
-  // res.end(JSON.stringify(jsonCal));
+  // res.end(JSON.stringify(newCalendar));
 
   // ENABLE FOR PRODUCTION
   newCalendar.serve(res);
---
+++ utils/parseUserConfig.js
@@ -0,0 +1,49 @@
+const parseUserConfig = (data) => {
+  /*
+    'B1_L1': 'https://calendar.google.com/calendar/ical/v0qbbfube10coopf5vfiovf5qo%40group.calendar.google.com/public/basic.ics',
+    'B1_L2': 'https://calendar.google.com/calendar/ical/c_gj0263fted8l7qfm15shi2069c%40group.calendar.google.com/public/basic.ics',
+    'B1_L3': 'https://calendar.google.com/calendar/ical/c_fsjo0lba0dfq79odbo46jer5fg%40group.calendar.google.com/public/basic.ics',
+    'dev': 'http://localhost:6969/basic.ics',
+  */
+  const config = {
+    'url': data.url ? data.url : 'https://calendar.google.com/calendar/ical/v0qbbfube10coopf5vfiovf5qo%40group.calendar.google.com/public/basic.ics',
+    'name': data.name ? data.name : 'My Calendar',
+    'type': 'blist',
+    'matchPart': data.matchPart ? data.matchPart : [
+      '[B1.09]',
+      '[B1.10]',
+      // "[B1.11]",
+      '[B1.12]',
+      '[B1.13]',
+      '[B1.14]',
+      '[B1.15]',
+      '[B1.16]',
+      'L1.1',
+      'L1.2',
+      'L1.3',
+      'L1.4',
+      // "L1.5",
+      'L1.6',
+      'L1.7',
+    ],
+    // 'matchString': data.matchString ? data.matchString : [
+    //   'English B1',
+    //   'Semester I',
+    //   'English',
+    // ],
+    // 'limit': {
+    //   'start': {
+    //     'year': 2021,
+    //     'month': 8,
+    //     'day': 1,
+    //   },
+    //   'end': {
+    //     'year': 2021,
+    //     'month': 12,
+    //     'day': 31,
+    //   },
+    // },
+  };
+  return config;
+};
+module.exports = parseUserConfig;
